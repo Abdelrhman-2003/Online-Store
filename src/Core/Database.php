09d60821f4471c086bf6c053dbc70fa4f1384aa9
Abdelrhman Khaled <113688080+Abdelrhman-2003@users.noreply.github.com>
2025-08-18 19:08:00 +0300

    Add query, fetch, fetchAll and execute methods to implement CRUD operations

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -6,7 +6,8 @@ use PDO;
 
 class Database
 {
-    protected $connection;
+    private $connection;
+    private $statement;
 
     public function __construct(array $config = [], string $username = "root", string $password = "")
     {
@@ -14,5 +15,28 @@ class Database
         $this->connection = new PDO($dsn, $username, $password, [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
+    }
+
+    private function query(string $sql, array $params = [])
+    {
+        $this->statement = $this->connection->prepare($sql);
+        $this->statement->execute($params);
+    }
+
+    public function fetchAll(string $sql, array $params = [])
+    {
+        $this->query($sql, $params);
+        return $this->statement->fetchAll();
+    }
+
+    public function fetch(string $sql, array $params = [])
+    {
+        $this->query($sql, $params);
+        return $this->statement->fetch();
+    }
+
+    public function execute(string $sql, array $params = [])
+    {
+        $this->query($sql, $params);
     }
 }
