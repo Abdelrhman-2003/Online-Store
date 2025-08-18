3ee6026ef63a45222a42d6580ae293a88c6aae59
Abdelrhman Khaled <113688080+Abdelrhman-2003@users.noreply.github.com>
2025-08-18 22:49:19 +0300

    Initialize DB Configuration

---
+++ src/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "default" => "mysql",
+    "connections" => [
+        "mysql" => [
+            "dbname" => "online_store",
+            "host" => "localhost",
+            "port" => 3306
+        ]
+    ]
+];
