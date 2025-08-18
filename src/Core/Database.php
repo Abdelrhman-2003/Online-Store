3a548c3649b58d3ba95ab5cd1e93d27a7a9799d0
Abdelrhman Khaled <113688080+Abdelrhman-2003@users.noreply.github.com>
2025-08-18 18:56:40 +0300

    Create Database Class and Initialize Connection with PDO

---
+++ src/Core/Database.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+
+class Database
+{
+    protected $connection;
+
+    public function __construct(array $config = [], string $username = "root", string $password = "")
+    {
+        $dsn = "mysql:" . http_build_query($config, "", ";");
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+}
