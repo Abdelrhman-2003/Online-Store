36761149d41c5809df54d97c67c01bee729b670f
Abdelrhman Khaled <113688080+Abdelrhman-2003@users.noreply.github.com>
2025-09-03 19:16:17 +0300

    Refactor show() method to improve Performance

--- src/Http/Controllers/CategoryController.php
+++ src/Http/Controllers/CategoryController.php
@@ -11,15 +11,14 @@ class CategoryController
 
     public function show(int $id)
     {
-        foreach ($this->getCategories() as $category) {
-            if ($category['id'] === $id) {
-                view("showCategory", [
-                    "products" => $this->getProducts($id),
-                    "category" => $this->getCategory($id),
-                    "categories" => $this->getCategories()
-                ]);
-                die();
-            }
+        if ($this->getCategory($id) != false) {
+
+            view("showCategory", [
+                "products" => $this->getProducts($id),
+                "category" => $this->getCategory($id),
+                "categories" => $this->getCategories()
+            ]);
+            die();
         }
         abort(404, "No other category found");
     }
@@ -27,17 +26,15 @@ class CategoryController
     private function getCategories()
     {
         return db()->fetchAll("SELECT * FROM categories");
-        
     }
 
     private function getCategory(int $id)
     {
         return db()->fetch("SELECT * FROM categories where id = ? LIMIT 1", [$id]);
-        
     }
-    
+
     private function getProducts(int $id)
     {
-         return db()->fetchAll("SELECT * FROM products where category_id = ?", [$id]);
+        return db()->fetchAll("SELECT * FROM products where category_id = ?", [$id]);
     }
 }
