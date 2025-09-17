a57b9d5e67b95457556821377ff2b756230684f7
Abdelrhman Khaled <113688080+Abdelrhman-2003@users.noreply.github.com>
2025-09-18 00:31:47 +0300

    multiple updates and fixes across the project

--- Public/index.php
+++ Public/index.php
@@ -1,13 +1,16 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 
 require __DIR__ . ("/../src/Core/Function.php");
 
 require base_path("Core/Router.php");
 
-use App\Core\Exception\FileNotFoundException;
 use App\Core\Exception\RecordNotFoundException;
-use App\Core\Exception\QueryException;
+use Exception;
 use Core\Router;
 
 $uri = parse_url($_SERVER['REQUEST_URI'])["path"];
@@ -19,13 +22,12 @@ require base_path("config/routes.php");
 
 try {
     $router->route($method, $uri);
-} catch (RuntimeException $e) {
-    abort(500, $e->getMessage());
 } catch (RecordNotFoundException $e) {
     abort(404, $e->getMessage());
+} catch (RuntimeException $e) {
+    serverError($e->getMessage(), $e->getFile(), $e->getLine());
 } catch (Exception $e) {
-    error_log($e->getMessage(), 3, __DIR__ . "/../logs/error.log");
-    abort(500, "Unexpected error, please try again later.");
+    serverError($e->getMessage(), $e->getFile(), $e->getLine());
 } finally {
     db()->disConnect();
 }
